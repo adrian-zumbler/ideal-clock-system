b02d1990bd5e84bff2ee6799da9dc74ec8101a5b
AdrianM <adrianm1@outlook.com>
2014-12-31 18:28:36 -0700

    Se agrego el controlodar add y el metedo de insercion mediante ajax

--- app/Controller/CheckoutsController.php
+++ app/Controller/CheckoutsController.php
@@ -5,4 +5,29 @@ class CheckoutsController extends AppController {
 	function index() {
 		
 	}
+
+	function add() {
+		//$this->request->onlyAllow('ajax');
+		if($this->request->is('post')){
+			$id =  $this->request->data['id'];
+			$data = array(
+			'Checkout' => array(
+				'time' => date("Y-m-d H:i:s",time()),
+				'user_id' => '1',
+				'auxiliar_id' => $id));
+			$this->Checkout->create();
+			$this->Checkout->save($data);	
+		}
+		
+
+
+		/*$this->request->onlyAllow('ajax');
+		if($this->request->is('post')) {
+			$this->Checkout->create();
+			if($this->Checkout->save()) {
+
+			}
+		}*/
+
+	}
 }
--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -5,12 +5,32 @@ $('.Checkout-button').click(function() {
 	name = $(this).text();
 	id = $(this).val();
 	lblstatus.innerHTML = name;
+	url = 'http://localhost:8080/ideal-clock-system/'
+	
+	$.ajax({
+		url: url + 'checkouts/add',
+		type: 'post',
+		data: {
+			'id': id
+		},
+		success: function() {
+			alert('Cambio de status exitoso')
+		},
+		error: function(error) {
+			alert(error.tostring)
+		} 
+			
+		
+	})
 });
+
 $('#available').click(function() {
 	name = $(this).text();
 	id = $(this).val();
 	lblstatus.innerHTML = name;
 });
+
+
 
 
 
